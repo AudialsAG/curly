35dbfbe581bcad76b03740467d7fe31b0e70684e
bobybacs <bobybacs@MacBookPro.local>
2018-04-23 10:56:07 +0300

    updated to curl 7.55

--- third-party/curl/ios/build_libcurl_dist.sh
+++ third-party/curl/ios/build_libcurl_dist.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#Based on https://github.com/sinofool/build-libcurl-ios
 
-VERSION="7.47.0"
+VERSION="7.55.0"
 CURRENTPATH=`pwd`
 
 export DEVROOT=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain
@@ -17,8 +16,9 @@ cd curl-${VERSION}
 
 function check_curl_ver() {
 echo "#include \"include/curl/curlver.h\"
-#if LIBCURL_VERSION_MAJOR < 7 || LIBCURL_VERSION_MINOR < 40
+#if LIBCURL_VERSION_MAJOR < 7 || LIBCURL_VERSION_MINOR < 55
 #error Required curl 7.40.0+; See http://curl.haxx.se/docs/adv_20150108A.html
+#error Supported minimal version is 7.55.0 for header file changes, see Issue #12 (https://github.com/sinofool/build-libcurl-ios/issues/12)
 #endif"|gcc -c -o /dev/null -xc -||exit 9
 }
 
@@ -27,13 +27,11 @@ function build_for_arch() {
   HOST=$2
   SYSROOT=$3
   PREFIX=$4
-  IPHONEOS_DEPLOYMENT_TARGET="6.0"
+  IPHONEOS_DEPLOYMENT_TARGET="9.0"
   export PATH="${DEVROOT}/usr/bin/:${PATH}"
-  #For now, turn off bitcode and change -Os to -Og
-  #export CFLAGS="-arch ${ARCH} -pipe -Os -gdwarf-2 -isysroot ${SYSROOT} -miphoneos-version-min=${IPHONEOS_DEPLOYMENT_TARGET} -fembed-bitcode"
-  export CFLAGS="-arch ${ARCH} -pipe -O0 -gdwarf-2 -isysroot ${SYSROOT} -miphoneos-version-min=${IPHONEOS_DEPLOYMENT_TARGET}"
+  export CFLAGS="-DCURL_BUILD_IOS -arch ${ARCH} -pipe -Os -gdwarf-2 -isysroot ${SYSROOT} -miphoneos-version-min=${IPHONEOS_DEPLOYMENT_TARGET} -fembed-bitcode"
   export LDFLAGS="-arch ${ARCH} -isysroot ${SYSROOT}"
-  ./configure --disable-shared --enable-static ${SSL_FLAG} --host="${HOST}" --prefix=${PREFIX} && make -j8 && make install
+  ./configure --disable-shared --without-zlib --enable-static --enable-ipv6 ${SSL_FLAG} --host="${HOST}" --prefix=${PREFIX} && make -j8 && make install
 }
 
 if [ "$1" == "openssl" ]
@@ -51,7 +49,7 @@ fi
 
 TMP_DIR=/tmp/build_libcurl_$$
 
-build_for_arch i386 i386-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/i386 || exit 1
+#build_for_arch i386 i386-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/i386 || exit 1
 build_for_arch x86_64 x86_64-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/x86_64 || exit 2
 build_for_arch arm64 arm-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/arm64 || exit 3
 build_for_arch armv7s armv7s-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/armv7s || exit 4
@@ -59,16 +57,13 @@ build_for_arch armv7 armv7-apple-darwin /Applications/Xcode.app/Contents/Develop
 
 mkdir -p ${TMP_DIR}/lib/
 ${DEVROOT}/usr/bin/lipo \
-	-arch i386 ${TMP_DIR}/i386/lib/libcurl.a \
 	-arch x86_64 ${TMP_DIR}/x86_64/lib/libcurl.a \
 	-arch armv7 ${TMP_DIR}/armv7/lib/libcurl.a \
 	-arch armv7s ${TMP_DIR}/armv7s/lib/libcurl.a \
 	-arch arm64 ${TMP_DIR}/arm64/lib/libcurl.a \
 	-output ${TMP_DIR}/lib/libcurl.a -create
 
-cp -r ${TMP_DIR}/armv7s/include ${TMP_DIR}/
-curl -O https://raw.githubusercontent.com/sinofool/build-libcurl-ios/master/patch-include.patch
-patch ${TMP_DIR}/include/curl/curlbuild.h < patch-include.patch
+cp -r ${TMP_DIR}/arm64/include ${TMP_DIR}/
 
 mkdir -p ${DIST_DIR}
 cp -r ${TMP_DIR}/include ${TMP_DIR}/lib ${DIST_DIR}
