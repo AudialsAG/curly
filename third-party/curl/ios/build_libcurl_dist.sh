caaabf2d1ef829c31ff9f575799253fba2c34cba
Robert Bacs <robert.bacs@audials.com>
2018-11-15 11:20:20 +0200

    enabled i386 architecture for legacy simulatore device

--- third-party/curl/ios/build_libcurl_dist.sh
+++ third-party/curl/ios/build_libcurl_dist.sh
@@ -49,7 +49,7 @@ fi
 
 TMP_DIR=/tmp/build_libcurl_$$
 
-#build_for_arch i386 i386-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/i386 || exit 1
+build_for_arch i386 i386-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/i386 || exit 1
 build_for_arch x86_64 x86_64-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/x86_64 || exit 2
 build_for_arch arm64 arm-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/arm64 || exit 3
 build_for_arch armv7s armv7s-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/armv7s || exit 4
@@ -57,6 +57,7 @@ build_for_arch armv7 armv7-apple-darwin /Applications/Xcode.app/Contents/Develop
 
 mkdir -p ${TMP_DIR}/lib/
 ${DEVROOT}/usr/bin/lipo \
+  -arch i386 ${TMP_DIR}/i386/lib/libcurl.a \
 	-arch x86_64 ${TMP_DIR}/x86_64/lib/libcurl.a \
 	-arch armv7 ${TMP_DIR}/armv7/lib/libcurl.a \
 	-arch armv7s ${TMP_DIR}/armv7s/lib/libcurl.a \
