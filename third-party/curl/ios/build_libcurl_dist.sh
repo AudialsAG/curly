4a83a0c45d76775166c71a451c5eed695dface9e
Robert Bacs <robert.bacs@audials.com>
2024-02-16 15:15:31 +0200

    updated libcurl to 8.6.0

--- third-party/curl/ios/build_libcurl_dist.sh
+++ third-party/curl/ios/build_libcurl_dist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="7.55.0"
+VERSION="8.6.0"
 CURRENTPATH=`pwd`
 
 export DEVROOT=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain
@@ -16,7 +16,7 @@ cd curl-${VERSION}
 
 function check_curl_ver() {
 echo "#include \"include/curl/curlver.h\"
-#if LIBCURL_VERSION_MAJOR < 7 || LIBCURL_VERSION_MINOR < 55
+#if LIBCURL_VERSION_MAJOR < 8 || LIBCURL_VERSION_MINOR < 6
 #error Required curl 7.40.0+; See http://curl.haxx.se/docs/adv_20150108A.html
 #error Supported minimal version is 7.55.0 for header file changes, see Issue #12 (https://github.com/sinofool/build-libcurl-ios/issues/12)
 #endif"|gcc -c -o /dev/null -xc -||exit 9
@@ -31,7 +31,7 @@ function build_for_arch() {
   export PATH="${DEVROOT}/usr/bin/:${PATH}"
   export CFLAGS="-DCURL_BUILD_IOS -arch ${ARCH} -pipe -Os -gdwarf-2 -isysroot ${SYSROOT} -miphoneos-version-min=${IPHONEOS_DEPLOYMENT_TARGET} -fembed-bitcode"
   export LDFLAGS="-arch ${ARCH} -isysroot ${SYSROOT}"
-  ./configure --disable-shared --without-zlib --enable-static --enable-ipv6 ${SSL_FLAG} --host="${HOST}" --prefix=${PREFIX} && make -j8 && make install
+  ./configure --disable-shared --without-zlib --without-libpsl --enable-static --enable-ipv6 ${SSL_FLAG} --host="${HOST}" --prefix=${PREFIX} && make -j8 && make install
 }
 
 if [ "$1" == "openssl" ]
@@ -44,12 +44,12 @@ then
   export SSL_FLAG=--with-ssl=${HOME}/Desktop/openssl-ios-dist
 else
   check_curl_ver
-  export SSL_FLAG=--with-darwinssl
+  export SSL_FLAG=--with-secure-transport
 fi
 
 TMP_DIR=/tmp/build_libcurl_$$
 
-build_for_arch i386 i386-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/i386 || exit 1
+# build_for_arch i386 i386-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/i386 || exit 1
 build_for_arch x86_64 x86_64-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/x86_64 || exit 2
 build_for_arch arm64 arm-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/arm64 || exit 3
 build_for_arch armv7s armv7s-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/armv7s || exit 4
@@ -57,7 +57,6 @@ build_for_arch armv7 armv7-apple-darwin /Applications/Xcode.app/Contents/Develop
 
 mkdir -p ${TMP_DIR}/lib/
 ${DEVROOT}/usr/bin/lipo \
-  -arch i386 ${TMP_DIR}/i386/lib/libcurl.a \
 	-arch x86_64 ${TMP_DIR}/x86_64/lib/libcurl.a \
 	-arch armv7 ${TMP_DIR}/armv7/lib/libcurl.a \
 	-arch armv7s ${TMP_DIR}/armv7s/lib/libcurl.a \
